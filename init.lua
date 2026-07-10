6f8c4310de53a3a59a8c5f25ed34f7d61d137697
gengibrepower <felipegegembauer>
2026-07-10 14:25:09 -0300

    gruvbox

--- init.lua
+++ init.lua
@@ -382,18 +382,23 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
-  ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
-    styles = {
-      comments = { italic = false }, -- Disable italics in comments
+vim.pack.add { gh 'ellisonleao/gruvbox.nvim' }
+  require('gruvbox').setup {
+    transparent_mode = true, -- vaza Normal/NormalFloat/SignColumn pro wallpaper
+    italic = {
+      comments = false, -- mantém comentários sem itálico, como você já fazia
+    },
+    overrides = {
+      -- garante o vazado nos grupos que o transparent_mode não cobre sozinho
+      Normal      = { bg = 'NONE' },
+      NormalNC    = { bg = 'NONE' }, -- splits inativos
+      NormalFloat = { bg = 'NONE' }, -- janelas flutuantes (lazy, mason, lsp hover)
+      SignColumn  = { bg = 'NONE' },
+      EndOfBuffer = { bg = 'NONE' }, -- área dos ~ abaixo do texto
     },
   }
-
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.o.background = 'dark' -- gruvbox tem variante light; fixa a dark
+  vim.cmd.colorscheme 'gruvbox'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
